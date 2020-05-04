46767d5749a77658bfdd35fa9bc7a0c047672ca6
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-05-04 16:39:08 +1000

    create new pic

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,13 +1,28 @@
 class PicsController < ApplicationController
   def index
   end
+  
+  def new
+    @pic = Pic.new
+  end
+  
+  def create
+    @pic = Pic.new(pic_params)
+  end
 
   def show
+
   end
 
   def edit
   end
 
-  def new
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
+  def pic_id
   end
 end
